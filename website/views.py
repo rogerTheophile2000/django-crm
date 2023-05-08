dccbbbe736f60a2cb54bf1f658519e65c9eaeb82
Roger Mushagalusa <rogetheophile2000@gmail.com>
2023-05-09 02:09:28 +0300

    adding login part

--- website/views.py
+++ website/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate,login, logout
+from django.contrib import messages
 
 # Create your views here.
 
 def home(request):
-    return render(request, "home.html", {})
+    #check to see if logging in
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        #auth
+        user = authenticate(request, username=username, password = password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "YOU have been loggged in!")
+            return redirect('home')
+        else:
+            messages.success(request, "I have an error logging, please try aigain")
+            return redirect('home')
+    else:
+        return render(request, "home.html", {})
+
+
+def log_out(request):
+    pass
