7b66ed152f346e0fdaae59668b58d252f70ea2c7
Roger Mushagalusa <rogetheophile2000@gmail.com>
2023-05-09 06:18:34 +0300

    adding CRD : first part : READ and DELETE

--- website/urls.py
+++ website/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     # path('login/', views.login_user, name="login"),
     path('logout/', views.log_out, name="logout"),
     path('register/', views.register_user, name="register"),
+    path('record/<int:pk>', views.customer_record, name="record"),
+    path('delete_record/<int:pk>', views.delete_customer, name="delete_record"),
 ]
--- website/views.py
+++ website/views.py
@@ -1,26 +1,30 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate,login, logout
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
+from .models import Record
 
 # Create your views here.
 
+
 def home(request):
-    #check to see if logging in
+    records = Record.objects.all()
+    # check to see if logging in
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
-        #auth
-        user = authenticate(request, username=username, password = password)
+        # auth
+        user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
             messages.success(request, "YOU have been loggged in!")
             return redirect('home')
         else:
-            messages.success(request, "I have an error logging, please try aigain")
+            messages.success(
+                request, "I have an error logging, please try aigain")
             return redirect('home')
     else:
-        return render(request, "home.html", {})
+        return render(request, "home.html", {'records': records})
 
 
 def log_out(request):
@@ -28,20 +32,41 @@ def log_out(request):
     messages.success(request, "YOU Have been logout successfully")
     return redirect('home')
 
+
 def register_user(request):
     if request.method == 'POST':
-        form  = SignUpForm(request.POST)
+        form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
             # Authenticate and login
             username = form.cleaned_data['username']
             password = form.cleaned_data['password1']
-            user = authenticate(username = username, password = password)
+            user = authenticate(username=username, password=password)
             login(request, user)
             messages.success(request, "You have successfully Register")
             return redirect('home')
     else:
         form = SignUpForm()
-        return render(request, "register.html", {'form' : form})
-    
-    return render(request, "register.html", {'form' : form})
+        return render(request, "register.html", {'form': form})
+
+    return render(request, "register.html", {'form': form})
+
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        customer_record = Record.objects.get(id=pk)
+        return render(request, "record.html", {'customer_record': customer_record})
+    else:
+        messages.success(request, "You login on view")
+        return redirect('home')
+
+
+def delete_customer(request, pk):
+    if request.user.is_authenticated:
+        delet_it = Record.objects.get(id=pk)
+        delet_it.delete()
+        messages.success(request, "Records deleted succcessfully!")
+        return redirect('home')
+    else:
+        messages.success(request, "you must be Logged in to do that...")
+        return redirect('home')
