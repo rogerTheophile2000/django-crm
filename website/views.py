7775e49b06c2870c616ac689eec2eaa618810bcb
Roger Mushagalusa <rogetheophile2000@gmail.com>
2023-05-09 06:58:37 +0300

    final project
    It's a final commit of the project with CRM app with django by CODEMY.COM

--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SignUpForm
+from .forms import SignUpForm, AddRecordForm
 from .models import Record
 
 # Create your views here.
@@ -67,6 +67,32 @@ def delete_customer(request, pk):
         delet_it.delete()
         messages.success(request, "Records deleted succcessfully!")
         return redirect('home')
+    else:
+        messages.success(request, "you must be Logged in to do that...")
+        return redirect('home')
+    
+def add_record(request):
+    form = AddRecordForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            if form.is_valid():
+                add_record = form.save()
+                messages.success(request, "record Added")
+                return redirect('home')
+        return render(request, "add_record.html", {'form': form})
+    else:
+        messages.success(request, "you must be Logged in to do that...")
+        return redirect('home')
+    
+def update_record(request, pk):
+    if request.user.is_authenticated:
+        current_record = Record.objects.get(id=pk)
+        form = AddRecordForm(request.POST or None, instance = current_record)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "record update")
+            return redirect('home')
+        return render(request, "update_record.html", {'form': form})
     else:
         messages.success(request, "you must be Logged in to do that...")
         return redirect('home')
